b03fde2c59765edce147b055b8d1cd442fee0a3b
Mohan Kumar BN <mkbn02@outlook.com>
2021-11-14 13:40:51 +0530

    added java8 compatibel code

--- src/main/java/mbn/spring/Application.java
+++ src/main/java/mbn/spring/Application.java
@@ -1,13 +1,13 @@
-package simplilearn.sprintboot;
+package mbn.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SprintBootApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SprintBootApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/mbn/spring/ApplicationTests.java
+++ src/test/java/mbn/spring/ApplicationTests.java
@@ -1,10 +1,10 @@
-package simplilearn.sprintboot;
+package mbn.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SprintBootApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
